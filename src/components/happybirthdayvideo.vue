c16d45de9d85454392099a543fdcb9688ede0dc9
eraynai <elli@cinehackers.com>
2021-03-05 14:21:46 -0500

    some small change

--- src/components/happybirthdayvideo.vue
+++ src/components/happybirthdayvideo.vue
@@ -2,7 +2,13 @@
     <section id="hero">
             <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
             <iframe
-                width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY">
+                width="421" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY">
             </iframe>
     </section>
 </template>
+
+<script>
+export default{
+
+}
+</script>

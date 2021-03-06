1fe2118864ec80c6330b452ae1597c8e395565d3
eraynai <elli@cinehackers.com>
2021-03-06 17:54:03 -0500

    watch now button connected to youtube api

--- src/components/happybirthdayvideo.vue
+++ src/components/happybirthdayvideo.vue
@@ -1,14 +1,115 @@
 <template>
-    <section id="hero">
+    <section class="happybirthday-video">
+        <div>
             <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h2>
-            <iframe
-                width="421" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY">
-            </iframe>
+            <p>lorem ipsDolore sint dolore enim cupidatat fugiat laboris magna laboris.Commodo aliquip enim voluptate sunt tempor aliqua ipsum culpa excepteur ad proident aute reprehenderit deserunt.</p>
+        </div>
+         <!-- Note that we use a `ref` attribute, not an `id`, to reference the iframe . The iframe src needs the enablejsapi flag as well, so that we can actually use the iFrame API.
+      It's also important that the iframe allows autoplay, or else the video will not play in some browsers -->
+          <iframe
+            ref="ytplayer"
+            width="560"
+            height="315"
+            allow="autoplay"
+            src="https://www.youtube-nocookie.com/embed/ppp5Q70PYPU?enablejsapi=1">
+        </iframe>
+         <!--We disable the play button until the player is ready.-->
+        <button @click="play" :disabled="!playerReady" class="watchnow-button">
+            <span>WATCH NOW</span>
+            <g-image src='~/assets/images/play_button.png' alt='play button'/>
+        </button>
+        
     </section>
 </template>
 
 <script>
-export default{
+/*
+  Here we load the YouTube iFrame API. This creates the `window.YT` global that gives us access to
+  the API. This script will only be loaded once.
+*/
+var tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+console.log("Youtube iFrame API script inserted.");
 
-}
+export default{
+    data: function () {
+        return {
+            playerReady: false,
+        };
+    },
+
+    mounted: function () {
+        console.log("YTVideo component mounted.");
+    /*
+      We need to use Vue's `mounted` lifecycle hook to wait for the component to be mounted
+      so that we can access the `ytplayer` ref.
+    */
+
+    /*
+      We create a ytAPIReady function that will be called later, when we've determined that
+      the Youtube iFrame API is actually available.
+      Note that it's important that we use arrow-style functions here, since we need access
+      to the vue component instance via the `this` keyword inside the functions.
+    */
+    const ytAPIReady = () => {
+        console.log("Youtube iFrame API ready");
+    /*
+    Now that the YouTube iFrame API is ready, we can use it to create an instance of the Player
+    object using our `ytplayer` reference.
+    */
+    console.log("Creating player instance.");
+    const _player = new window.YT.Player(this.$refs.ytplayer, {
+        events: {
+            onReady: (event) => {
+                console.log("Player ready");
+            /*
+              The `playVideo` method on the Player object is not available until the player has entered
+              the `ready` state, so we need to wait for the `onReady` event to occur.
+              We can then store the player instance on the component for use in the `play` method below.
+            */
+
+            this._player = _player;
+             /*
+              Now that the player instance is available and ready, we can flip our `playerReady` flag, which
+              will enable the play button.
+            */
+            this.playerReady = true;
+            },
+        },
+    });
+};
+
+/*
+      We determine if the YouTube iFrame API is ready by checking for the existance of the `YT` global.
+      If the API is not available, we need to wait for it before calling ytAPIReady.
+      Since we add the YTVideo to the page after a button click, the API should typically be available
+      already. If you set `showVideo` to `true` by default in `App.vue`, you'll see that we need to wait
+      for the API to be available.
+*/
+    if (window.YT) {
+      console.log("The YouTube iFrame API is already available.");
+      ytAPIReady();
+    } else {
+      console.log(
+        "The YouTube iFrame API is not available yet. Using onYoutubeIframeAPIReady to wait for it."
+      );
+
+      window.onYouTubeIframeAPIReady = function () {
+        console.log("The YouTube iFrame API is now available.");
+        ytAPIReady();
+      };
+    }
+  },
+ methods: {
+    play: function () {
+      console.log("Playing video.");
+      /*  
+        Finally, we can use the `_player` reference to play the video.
+      */
+      this._player.playVideo();
+    },
+  },
+};
 </script>

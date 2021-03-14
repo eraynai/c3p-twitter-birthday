36d29979035ad6ee6378a17305fd45899769c258
Elli Raynai <elli@cinehackers.com>
2021-03-14 08:38:50 -0400

    styled text about video section

--- src/components/calltoaction.vue
+++ src/components/calltoaction.vue
@@ -2,7 +2,7 @@
     <div class="inner-background-cta">
         <section class="cta-section wrapper">
             <div class="cta-content">
-                <p>We offered to proactively detect and report known child sexual abuse materials (CSAM) being shared on their platform – at no cost to them – but they declined</p>
+                <p>We offered to proactively detect and report known child sexual abuse materials (CSAM) being shared on their platform – at no cost to them – but they declined.</p>
                 <p>They do not provide users with the ability to directly report tweets or DM as containing child sexual abuse material, which we believe increases the time it stays on the site and allows it to spread.</p>
                 <p>This needs to change.</p>
             </div>

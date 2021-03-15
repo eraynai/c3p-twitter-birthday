2dd7510c3404167081e73f5bf735130e810fe143
Elli Raynai <elli@cinehackers.com>
2021-03-15 07:38:33 -0400

    set styling for footer on desktop and mobile screens

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,16 +1,24 @@
 <template>
     <div class="wrapperWide">
     <footer class="footerContent wrapper">
+    <div class="container"> 
         <div class="social-logos">
-    
-            
-            
-            <g-image src='~/assets/images/youtube_1.svg' alt='thumb'/>
-            
+            <g-image class="cp3-logo" src="~/assets/images/c-3-p-logo.svg" alt="c3p logo" />
+            <div class="social">
+                <a href="#"><g-image src="~/assets/images/facebook-white.svg" alt="facebook icon" /></a>
+                <a href="#"><g-image src="~/assets/images/instagram-white.svg" alt="instagram icon" /></a>
+                <a href="#"><g-image src="~/assets/images/twitter-white_v2.svg" alt="twitter icon" /></a>
+                <a href="#"><g-image class="youIcon" src="~/assets/images/youtube-white_v2.svg" alt="youtube icon" /></a>
+            </div>
         </div>
-        <p>Privacy</p>
-        <p>Terms Of Use</p>
-        <p>Contact Us</p>
+         <div class="terms">
+             <ul>
+                 <li><a href="#">PRIVACY</a></li>
+                 <li><a href="#">TERMS OF USE</a></li>
+                 <li><a href="#">CONTACT US</a> </li>
+             </ul>
+         </div>
+    </div>
     </footer>
     </div>
 </template>

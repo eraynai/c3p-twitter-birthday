ca6e935d115ed3c850f2b9b4226a9a420c904bc3
Elli Raynai <elli@cinehackers.com>
2021-03-15 15:59:58 -0400

    links pushed

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -5,17 +5,17 @@
         <div class="social-logos">
             <g-image class="cp3-logo" src="~/assets/images/c-3-p-logo.svg" alt="c3p logo" />
             <div class="social">
-                <a href="#"><g-image src="~/assets/images/facebook-white.svg" alt="facebook icon" /></a>
-                <a href="#"><g-image src="~/assets/images/instagram-white.svg" alt="instagram icon" /></a>
-                <a href="#"><g-image src="~/assets/images/twitter-white_v2.svg" alt="twitter icon" /></a>
-                <a href="#"><g-image class="youIcon" src="~/assets/images/youtube-white_v2.svg" alt="youtube icon" /></a>
+                <a href="https://www.facebook.com/CanadianCentreForChildProtection/" target="_blank"><g-image src="~/assets/images/facebook-white.svg" alt="facebook icon" /></a>
+                <a href="https://www.instagram.com/cdnchildprotect" target="_blank"><g-image src="~/assets/images/instagram-white.svg" alt="instagram icon" /></a>
+                <a href="https://twitter.com/cdnchildprotect" target="_blank"><g-image src="~/assets/images/twitter-white_v2.svg" alt="twitter icon" /></a>
+                <a href="https://www.youtube.com/user/ProtectChildrenCA" target="_blank"><g-image class="youIcon" src="~/assets/images/youtube-white_v2.svg" alt="youtube icon" /></a>
             </div>
         </div>
          <div class="terms">
              <ul>
-                 <li><a href="#">PRIVACY</a></li>
-                 <li><a href="#">TERMS OF USE</a></li>
-                 <li><a href="#">CONTACT US</a> </li>
+                 <li><a href="https://www.protectchildren.ca/en/privacy-policy/" target="_blank">PRIVACY</a></li>
+                 <li><a href="https://www.protectchildren.ca/en/terms-of-use/" target="_blank">TERMS OF USE</a></li>
+                 <li><a href="https://www.protectchildren.ca/en/contact/" target="_blank">CONTACT US</a> </li>
              </ul>
          </div>
     </div>

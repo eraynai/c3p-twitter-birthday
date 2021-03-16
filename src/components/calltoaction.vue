168144f9fc9c5945baf7d92d21da3c3d9bb6a103
Elli Raynai <elli@cinehackers.com>
2021-03-16 13:43:45 -0400

    background image of call to action swapped and spacing given to paragraph text on call to action

--- src/components/calltoaction.vue
+++ src/components/calltoaction.vue
@@ -2,12 +2,17 @@
     <div class="inner-background-cta">
         <section class="cta-section wrapper">
             <div class="cta-content">
-                
+                <div class="one"> 
                 <p>We offered to proactively detect and report known child sexual abuse materials (CSAM)<br>being
                 shared on their platform – at no cost to them – but they declined.</p>
+                </div>
+                <div class="one">
                 <p>They do not provide users with the ability to directly report tweets or DM as containing<br>
                 child sexual abuse material, which we believe allows it to spread because it increases the time it stays on the site.</p>
+                </div>
+                <div class="one">
                 <p>Now is the time to call on Twitter, and all other online platforms, to take action and prioritize the removal of child sexual abuse materials.</p>
+                </div>
             </div>
         </section>
     </div>

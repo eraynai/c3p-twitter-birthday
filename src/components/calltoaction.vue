13f188f2bad8bd26e844f0d1cb1e932c8e6a7567
Elli Raynai <elli@cinehackers.com>
2021-03-16 14:22:00 -0400

    fixed text and put spacing between the paragraphs

--- src/components/calltoaction.vue
+++ src/components/calltoaction.vue
@@ -3,14 +3,14 @@
         <section class="cta-section wrapper">
             <div class="cta-content">
                 <div class="one"> 
-                <p>We offered to proactively detect and report known child sexual abuse materials (CSAM)<br>being
+                <p>We offered to proactively detect and report known child sexual abuse materials (CSAM) being
                 shared on their platform – at no cost to them – but they declined.</p>
                 </div>
                 <div class="one">
-                <p>They do not provide users with the ability to directly report tweets or DM as containing<br>
+                <p>They do not provide users with the ability to directly report tweets or DM as containing
                 child sexual abuse material, which we believe allows it to spread because it increases the time it stays on the site.</p>
                 </div>
-                <div class="one">
+                <div class="three">
                 <p>Now is the time to call on Twitter, and all other online platforms, to take action and prioritize the removal of child sexual abuse materials.</p>
                 </div>
             </div>

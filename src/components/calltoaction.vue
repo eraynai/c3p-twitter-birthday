7cf53f7afd83dd4e62e4b9077aca8a2f63c46173
Elli Raynai <elli@cinehackers.com>
2021-03-17 13:39:11 -0400

    call to action text created for french and english

--- src/components/calltoaction.vue
+++ src/components/calltoaction.vue
@@ -3,15 +3,13 @@
         <section class="cta-section wrapper">
             <div class="cta-content">
                 <div class="one"> 
-                <p>We offered to proactively detect and report known child sexual abuse materials (CSAM) being
-                shared on their platform – at no cost to them – but they declined.</p>
+                <p>{{$t('call_to_action_p_one')}}</p>
                 </div>
                 <div class="one">
-                <p>They do not provide users with the ability to directly report tweets or DM as containing
-                child sexual abuse material, which we believe allows it to spread because it increases the time it stays on the site.</p>
+                <p>{{$t('call_to_action_p_two')}}</p>
                 </div>
                 <div class="three">
-                <p>Now is the time to call on Twitter, and all other online platforms, to take action and prioritize the removal of child sexual abuse materials.</p>
+                <p>{{$t('call_to_action_p_three')}}</p>
                 </div>
             </div>
         </section>
@@ -20,7 +18,7 @@
 
 <script>
 export default {
-    /*{{$t('call_to_action_p_two')}}*/
+    
 }
 </script>
 

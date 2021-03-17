f056be14c5b65e74d3684cf344e8ddadff1ca422
Elli Raynai <elli@cinehackers.com>
2021-03-17 14:15:44 -0400

    put translated texted in footer

--- src/components/candlemap.vue
+++ src/components/candlemap.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="heroContent-2 wrapper">
         <div class="hero-content-2">
-            <p>A single tweet can spread across the entire globe in as fast as 15 seconds. This is why we need them to implement better policies to ensure that known child sexual abuse material doesn’t end up on their site, and newly identified materials get removed as quickly as possible when reported.</p> 
+            <p>{{$t('candle_des')}}</p> 
         </div>
     </section>
 </template>
--- src/components/footer.vue
+++ src/components/footer.vue
@@ -13,9 +13,9 @@
         </div>
          <div class="terms">
              <ul>
-                 <li><a href="https://www.protectchildren.ca/en/privacy-policy/" target="_blank">PRIVACY</a></li>
-                 <li><a href="https://www.protectchildren.ca/en/terms-of-use/" target="_blank">TERMS OF USE</a></li>
-                 <li><a href="https://www.protectchildren.ca/en/contact/" target="_blank">CONTACT US</a> </li>
+                 <li><a href="https://www.protectchildren.ca/en/privacy-policy/" target="_blank">{{$t('privacy')}}</a></li>
+                 <li><a href="https://www.protectchildren.ca/en/terms-of-use/" target="_blank">{{$t('terms')}}</a></li>
+                 <li><a href="https://www.protectchildren.ca/en/contact/" target="_blank">{{$t('contact')}}</a> </li>
              </ul>
          </div>
     </div>
